480d9d29224db30f79784541858bedcafac03237
Jake Day <jaked97@gmail.com>
2018-10-31 22:53:21 -0700

    Remove unneeded files

--- ABD/signup_action.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../src/login.php";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "Form Submitted" . "<br>";
-    create_user($_POST["first"], $_POST["last"], $_POST["degree"], $_POST["year"], $_POST["email"], $_POST["password"], "Visitor", 0);
-}
-?>
