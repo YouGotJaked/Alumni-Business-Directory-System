236c5d63edf28d261838b60051d264b46436b881
Jake Day <jaked97@gmail.com>
2018-10-31 17:31:30 -0700

    Move logic from entry to login module

--- src/login.php
+++ src/login.php
@@ -5,7 +5,7 @@ include "user.php";
 function create_user($first_name, $last_name, $degree, $graduation_year, $email, $password, $role, $business_id) {
     $user = new User();
     $exists = $user->get_one('email', $email);  // check if user already exists with that email
-    if ($exists) {
+    if ($exists != '[]') {
         echo "User already exists" . "<br>";
         return;
     }
@@ -34,9 +34,6 @@ function create_user($first_name, $last_name, $degree, $graduation_year, $email,
 function login($email, $password) {
     $user = new User();
     $json = $user->get_one('email', $email);
-    if ($json == '[]') {
-        return FALSE; // empty JSON array
-    }
     $json_obj = json_decode($json);
     return password_verify($password, $json_obj[0]->hashed_password);
 }
