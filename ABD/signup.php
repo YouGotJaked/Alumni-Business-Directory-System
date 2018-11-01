b9e4c4ad846520c756d6911cc8dac47e9a139c6d
Jake Day <jaked97@gmail.com>
2018-10-31 18:25:58 -0700

    Move Ye's

--- ABD/signup.php
+++ ABD/signup.php
@@ -59,10 +59,8 @@
     <?php
     include "../src/login.php";
     if (isset($_POST['submit'])) {
-        create_user($_POST["first"], $_POST["last"], $_POST["degree"], $_POST["year"], $_POST["email"], $_POST["password"], "Visitor", 0);
-    }
-    if (isset($_POST)) {
         echo "YE YE YE" . "<br>";
+        create_user($_POST["first"], $_POST["last"], $_POST["degree"], $_POST["year"], $_POST["email"], $_POST["password"], "Visitor", 0);
     }
     ?>
 </body>
