f76d6534545cc38604fe1a06e4b99241fdd3b355
Jake Day <jaked97@gmail.com>
2018-11-12 21:17:05 -0800

    Move css files to new dir

--- web/manager_home.php
+++ web/manager_home.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<link href="styles.css" rel="stylesheet" type="text/css"/>
+	<link href="../css/styles.css" rel="stylesheet" type="text/css"/>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
