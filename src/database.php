41c208a1a1359f48f3f665ff90fa80ade58f26be
Jake Day <jaked97@gmail.com>
2018-11-12 20:10:47 -0800

    Move db credentials to .ini

--- src/database.php
+++ src/database.php
@@ -1,14 +1,14 @@
 <?php
 class Database {
-    public $host = "dbserver.engr.scu.edu";
-    public $user = "pahrens";
-    public $pass = "alUmni!43255BuSine33*";
-    public $name = "sdb_pahrens";
-    public $conn;
+    static $conn;
 
     public function connect() {
-        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->name);
-
+        // Connect to database if connection not yet established
+        if (!isset($conn)) {
+            $config = parse_ini_file("../local/config.ini");  // retrieve db credentials
+            $this->conn = mysqli_connect($config["host"], $config["user"], $config["pass"], $config["name"]);
+        }
+        
         if ($this->conn->connect_error) {
             die();
             return FALSE;
