5e65af6cabb6aa61abb2104aaa0553628a117abc
pwahrens <pwahrens@gmail.com>
2018-11-19 14:47:16 -0800

    [server] Fix individual business page after messy merge

--- web/individual_business.php
+++ web/individual_business.php
@@ -8,6 +8,7 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="../js/scripts.js"></script>
 	<title>Alumni Business Directory</title>
 </head>
 
@@ -41,9 +42,39 @@
 		<h1>Santa Clara University Business Directory</h1>
 	</div>
 	<div class="container text-center">
-		<h1>Business: </h1>
-		<p class="col-6 mx-auto">Description: </p>
-		<p>Address: <a href="...">...</a></p>
+		<h1 id="business-name"></h1>
+		<p id="business-description" class="col-6 mx-auto"></p>
+		<div id="business-category"></div>
+		<div id="business-street"></div>
+		<div id="business-city"></div>
+		<div id="business-state"></div>
+		<div id="business-zip"></div>
+		<div id="business-country"></div>
 	</div>
+
+	<?php
+
+	include "../src/business.php";
+			
+	// Verify user is logged in
+	//if (!$_SESSION['login']) {
+	//	header('Location: login.php');
+	//}
+
+	$business = new Business();
+
+	if (isset($_GET['business_id'])) {
+		$individual_business = $business->get_one("id", $_GET['business_id']);
+	} else {
+		$individual_business = 0;
+	}
+
+	?>
+
+	<script>
+		$(function () {
+        	populateIndividualBusinessFields(<?php echo $individual_business ?>)
+    	});
+	</script>
 </body>
 </html>
