8907611f42fb3fa2f9d81d092fc443e0ec62f71c
Simon Stauber <staubsfour@gmail.com>
2018-11-26 16:14:41 -0800

    reformatting

--- web/individual_business.php
+++ web/individual_business.php
@@ -49,12 +49,14 @@
 	<div class="container text-center col-lg-6 col-sm-10">
 		<h1 id="business-name"></h1>
 		<p id="business-description" class="mx-auto"></p>
-		<div id="business-category"></div>
-		<div id="business-street"></div>
-		<div id="business-city"></div>
-		<div id="business-state"></div>
-		<div id="business-zip"></div>
-		<div id="business-country"></div>
+		<div id="business-category" class= "mb-2 text-uppercase"></div>
+		<div class="form-row">
+			<div id="business-street" class="col"></div>
+			<div id="business-city" class="col"></div>
+			<div id="business-state" class="col"></div>
+			<div id="business-zip" class="col"></div>
+			<div id="business-country" class="col"></div>
+		</div>
 	</div>
 
 	<?php
