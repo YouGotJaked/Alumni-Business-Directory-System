15bd5ae91bac082322c2e1b981291baa70b59b74
Jake Day <jaked97@gmail.com>
2018-11-27 14:50:54 -0800

    Remove extra char

--- web/edit_business.php
+++ web/edit_business.php
@@ -79,7 +79,7 @@
             <div class="form-row">
                 <div class="form-group col-lg col-sm-12 mb-3">
                     <label for="exampleFormControlTextarea1">Business Description</label>
-                    <textarea class="form-control" name="description">'<?php print htmlentities($busn->description, ENT_QUOTES); ?>'></textarea>
+                    <textarea class="form-control" name="description">'<?php print htmlentities($busn->description, ENT_QUOTES); ?>'</textarea>
                 </div>
             </div>
             <div class="form-row">
@@ -97,7 +97,7 @@
             <div class="form-row">
                 <div class="col-lg col-sm-12 mb-3">
                     <label for="validationServer01">Street</label>
-                    <input type="text" name="street" class="form-control" value='<?php print htmlentities($busn->street, ENT_QUOTES); ?>'>
+                    <input type="text" name="street" class="form-control" value='<?php print htmlentities($busn->street, ENT_QUOTES); ?>'
                 </div>
                 <div class="col-lg-4 col-sm-12 mb-3">
                     <label for="validationServer01">City</label>
