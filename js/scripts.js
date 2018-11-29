5e0eff9521015c56a797f97b4f434579184ef0a6
Jake Day <jaked97@gmail.com>
2018-11-29 00:09:00 -0800

    Add case-insensitive search

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@ function populateBusinessList(approved, name, category, city) {
     for (var i = 0; i < approved.length; ++i) {
         var bob = approved[i].name.toUpperCase();
         console.log(bob);
-        if (approved[i].name.includes(name.toUpperCase()) && approved[i].category.includes(category) && approved[i].city.includes(city)) {
+        if (bob.includes(name.toUpperCase()) && approved[i].category.includes(category) && approved[i].city.includes(city)) {
             results.push(approved[i])
         }
     }
