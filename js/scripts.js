aa0d541dd5d7f87c99a07e6ab4f7c20a053d0bee
Jake Day <jaked97@gmail.com>
2018-11-29 00:08:33 -0800

    Add log

--- js/scripts.js
+++ js/scripts.js
@@ -45,8 +45,8 @@ function populateBusinessList(approved, name, category, city) {
     results = []
     //approved = $.map(approved, String.toUpperCase);
     for (var i = 0; i < approved.length; ++i) {
-        approved[i].name.toUpperCase();
-        console.log(approved[i].name);
+        var bob = approved[i].name.toUpperCase();
+        console.log(bob);
         if (approved[i].name.includes(name.toUpperCase()) && approved[i].category.includes(category) && approved[i].city.includes(city)) {
             results.push(approved[i])
         }
