ef0ca23aab0ac2403497c062fb34566c95d864b3
pwahrens <pwahrens@gmail.com>
2018-11-28 21:44:05 -0800

    [all] Commenting and permissions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+/* 
+    DESCRIPTION: This file handles functionality for the individual business and users pages as well as the 
+    business and user list pages. The functions mainly add new content to the page.
+*/
+
+/* 
+    NAME: populateIndividualBusinessFields
+    PARAMETERS:
+        business (Array): An array with a single JSON object representing the business details
+        owner (Array): An array with a single JSON object representing the owner details
+    RETURNS: none
+    DESCRIPTION: When the individual business page loads, this function will take business and owner data and populate the 
+        corresponding div elements in the DOM with the correct information.
+*/
 function populateIndividualBusinessFields(business, owner) {
     business = business[0]
     owner = owner[0]
@@ -15,6 +29,18 @@ function populateIndividualBusinessFields(business, owner) {
     $(".container").append('<a href="individual_user.php?user_id=' + owner.id + '" id="business-owner">' + owner.first_name + " " + owner.last_name + '</a>')
 }
 
+/* 
+    NAME: populateBusinessList
+    PARAMETERS:
+        approved (Array): An array with multiple JSON objects representing businesses
+        name (String): A string of the name the user searched for
+        category (String): A string of the category the user searched for
+        city (String): A string of the city the user searched for
+    RETURNS: none
+    DESCRIPTION: This function takes an array of all approved businesses from the database and creates new cards in the HTML body.
+        Each of the cards have some information about the business. The actor can click on the card to access even more information. 
+        The cards are only added to the HTML body if their data matches the name, category, and city parameters.
+*/
 function populateBusinessList(approved, name, category, city) {
     results = []
 
@@ -38,6 +64,15 @@ function populateBusinessList(approved, name, category, city) {
     }
 }
 
+/* 
+    NAME: populateIndividualUserFields
+    PARAMETERS:
+        user (Array): An array with a single JSON object representing the user details
+        business (Array): An array with a single JSON object representing the business details
+    RETURNS: none
+    DESCRIPTION: When the individual user page loads, this function will take user and business data and populate the 
+        corresponding div elements in the DOM with the correct information.
+*/
 function populateIndividualUserFields(user, business) {
     user = user[0]
     
@@ -55,6 +90,14 @@ function populateIndividualUserFields(user, business) {
     }
 }
 
+/* 
+    NAME: populateUserList
+    PARAMETERS:
+        users (Array): An array with multiple JSON objects representing users
+    RETURNS: none
+    DESCRIPTION: This function takes an array of all the users from the database and creates new cards in the HTML body.
+        Each of the cards have some information about the user. The actor can click on the card to access even more information.
+*/
 function populateUserList(users) {
     for (var i = 0; i < users.length; ++i) {
         var card_body_id = "user".concat('_', users[i].id)
--- src/entry.php
+++ src/entry.php
@@ -1,14 +1,36 @@
 <?php
+
+/* 
+    DESCRIPTION: This class is used to interact with the business and user tables located in the database. 
+        The Business and User classes extend this class.
+*/
+
 require_once "database.php";
 
 class Entry {
     public $table;
     public $database;
         
+    /* 
+    NAME: __construct
+    PARAMETERS: none
+    RETURNS: none
+    DESCRIPTION: 
+        This function initializes the public database variable
+    */
     public function __construct() {
         $this->database = new Database();
     }
         
+    /* 
+    NAME: get_one
+    PARAMETERS:
+        $key (String): The database column with which you are trying to match a value
+        $value (String or Int): The value you are checking the key for
+    RETURNS: The results of this query packaged into JSON
+    DESCRIPTION: 
+        This function does a key-value search in the database and returns a single result packaged as JSON
+    */
     public function get_one($key, $value) {
         $this->database->connect();
         
@@ -29,6 +51,15 @@ class Entry {
         return $json_obj;
     }
 
+    /* 
+    NAME: get_all
+    PARAMETERS:
+        $key (String): The database column with which you are trying to match a value
+        $value (String or Int): The value you are checking the key for
+    RETURNS: The results of this query packaged into JSON
+    DESCRIPTION: 
+        This function does a key-value search in the database and returns one or many results packaged as JSON
+    */
     public function get_all($key, $value) {
         $this->database->connect();
 
@@ -55,6 +86,16 @@ class Entry {
         return $json_obj;
     }
 
+    /* 
+    NAME: update
+    PARAMETERS:
+        $id (Int): The id of the database entry you wish to update
+        $key (Int): The database column with which you are trying to update a value
+        $value (Int): The new value with which to replace the old value in the key
+    RETURNS: True if the entry was updated, false if it was not updated 
+    DESCRIPTION: 
+        This function updates a single value for a single entry in the database.
+    */
     public function update($id, $key, $value) {
         $this->database->connect();
         $query = "UPDATE $this->table SET $key='$value' WHERE id='$id'";
@@ -64,6 +105,14 @@ class Entry {
         return $resp;
     }
 
+    /* 
+    NAME: remove
+    PARAMETERS:
+        $id (Int): The id of the entry you wish to remove
+    RETURNS: True if the entry was removed, false if it was not removed
+    DESCRIPTION: 
+        This function removes a single entry from the database 
+    */
     public function remove($id) {
         $this->database->connect();
         $query = "DELETE FROM $this->table WHERE id='$id'";
