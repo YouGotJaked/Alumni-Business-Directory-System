920457e65e898a2511ec243c8ca111f4958fcade
Simon Stauber <staubsfour@gmail.com>
2018-11-28 22:40:45 -0800

    left text

--- web/individual_business.php
+++ web/individual_business.php
@@ -65,7 +65,7 @@
 	<div class="card border-dark container col-lg-6 col-sm-10 p-3">
 		<u><h1 id="business-name" class="text-uppercase text-center"></h1></u>
 		<div id="business-category" class= "mb-3 text-uppercase text-center"></div>
-		<p id="business-description" class="mx-auto"></p>
+		<p id="business-description"></p>
 			<div id="business-street"></div>
 			<div id="business-city"></div>
 			<div id="business-state"></div>
