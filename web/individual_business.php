a80c25f935cd849ac10c4a1d38426f1422fffa64
Simon Stauber <staubsfour@gmail.com>
2018-11-28 16:49:15 -0800

    p to span

--- web/individual_business.php
+++ web/individual_business.php
@@ -65,7 +65,11 @@
 		<u><h1 id="business-name" class="text-uppercase text-center"></h1></u>
 		<div id="business-category" class= "mb-3 text-uppercase text-center"></div>
 		<p id="business-description" class="mx-auto"></p>
-		<div id="business-street business-city business-state business-zip business-country"></div>
+		<span id="business-street"></span>
+		<span id="business-city"></span>
+		<span id="business-state"></span>
+		<span id="business-zip"></span>
+		<span id="business-country"></span>
 	</div>
 
 	<?php
